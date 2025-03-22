e11ee4ff26b8b9f6dbe94daa4cc66784b0b7fa2a
RasskazovRuslan <ruslan.rasskazov@pedant.ru>
2025-03-22 15:45:15 +0500

    v

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -25,7 +25,7 @@ echo -e "\n--Инициализация первого шарда--\n"
 docker exec -i rr-shard-one mongosh --port 27018 --quiet <<EOF
 rs.initiate(
   {
-    _id:"rr-shard-one-test",
+    _id:"rr-shard-one",
     members: [
       {
         _id: 0, host:"rr-shard-one:27018",
